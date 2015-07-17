331699edd5a7ec1fe06303fbc8cb54a6db2ee815
Seán Mac Dónaill <johnmcdonnell89@gmail.com>
2015-07-17 02:40:39 +0100

    forgot to add

---
+++ colorblind-nwf/app/levels/basic-tileset.tsx
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="basic-tileset" tilewidth="20" tileheight="20">
+ <tile id="0">
+  <image width="20" height="20" source="basic-tileset/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="20" height="20" source="basic-tileset/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="20" height="20" source="basic-tileset/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="20" height="20" source="basic-tileset/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="20" height="20" source="basic-tileset/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="20" height="20" source="basic-tileset/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="20" height="20" source="basic-tileset/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="20" height="20" source="basic-tileset/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="20" height="20" source="basic-tileset/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="20" height="20" source="basic-tileset/10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="20" height="20" source="basic-tileset/11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="20" height="20" source="basic-tileset/12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="20" height="20" source="basic-tileset/13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="20" height="20" source="basic-tileset/14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="20" height="20" source="basic-tileset/15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="20" height="20" source="basic-tileset/16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="20" height="20" source="basic-tileset/17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="20" height="20" source="basic-tileset/18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="20" height="20" source="basic-tileset/19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="20" height="20" source="basic-tileset/20.png"/>
+ </tile>
+ <tile id="20">
+  <image width="20" height="20" source="basic-tileset/21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="20" height="20" source="basic-tileset/22.png"/>
+ </tile>
+ <tile id="22">
+  <image width="20" height="20" source="basic-tileset/23.png"/>
+ </tile>
+ <tile id="23">
+  <image width="20" height="20" source="basic-tileset/24.png"/>
+ </tile>
+ <tile id="24">
+  <image width="20" height="20" source="basic-tileset/25.png"/>
+ </tile>
+ <tile id="25">
+  <image width="20" height="20" source="basic-tileset/26.png"/>
+ </tile>
+</tileset>
